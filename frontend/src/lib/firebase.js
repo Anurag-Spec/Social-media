f25c0f1d35a6d7c07fa6fbdf4b8c5caac3324794
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-29 19:05:07 +0530

    feat: added firebase functions

---
+++ frontend/src/lib/firebase.js
@@ -0,0 +1,13 @@
+import Firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {};
+
+const firebase = Firebase.initializeApp(config);
+
+const { FieldValue } = Firebase.firestore;
+
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
