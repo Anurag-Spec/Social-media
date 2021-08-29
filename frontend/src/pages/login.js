ed39aa632ae6b685ace2623e31fe78ce6529e5b5
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-29 21:01:04 +0530

    feat: added context and state to login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 
-function login() {
+function Login() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const history = useHistory();
+  const isInvalid = password === "" || emailAddress === "";
+  const { firebase } = useContext(FirebaseContext);
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - TwinGram";
+  }, []);
   return <div>Login Page</div>;
 }
 
-export default login;
+export default Login;
