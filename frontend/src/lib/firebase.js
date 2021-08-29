a71b16ed80f36fc1364cc07be4dacc2d837d1835
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-29 19:19:47 +0530

    feat: added data to firebase

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -2,12 +2,17 @@ import Firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-const config = {};
+const config = {
+  apiKey: "AIzaSyDRXmw8Q8gfXabpMRf2y3F7a7kUEeM9VZU",
+  authDomain: "twingram-69cf7.firebaseapp.com",
+  projectId: "twingram-69cf7",
+  storageBucket: "twingram-69cf7.appspot.com",
+  messagingSenderId: "394747320886",
+  appId: "1:394747320886:web:3565cf5ee4889a91895697",
+};
 
 const firebase = Firebase.initializeApp(config);
 
 const { FieldValue } = Firebase.firestore;
-
-// seedDatabase(firebase);
 
 export { firebase, FieldValue };
