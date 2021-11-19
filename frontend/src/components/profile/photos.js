f244d99795bfc9ffe1fb97ece066adff89e65bd8
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-11-19 09:59:14 +0530

    fix: extra gray back on hover

--- frontend/src/components/profile/photos.js
+++ frontend/src/components/profile/photos.js
@@ -46,8 +46,12 @@ export default function Photos({ photos, user }) {
                 ) : null}
 
                 <div className="relative group">
-                  <img src={photo.imageSrc} alt={photo.caption} />
-                  <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex hidden">
+                  <img
+                    className="w-60"
+                    src={photo.imageSrc}
+                    alt={photo.caption}
+                  />
+                  <div className="absolute bottom-0 left-0 bg-gray-150 z-10 items-center w-60  justify-evenly h-full bg-black-faded group-hover:flex hidden">
                     <p className="flex items-center text-white font-bold">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
